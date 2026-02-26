4d66ee2f2c0f1d1cba68a728534b16faeed66cf6
vsiffermann <vsiffermann@gmail.com>
2026-02-26 18:44:59 +0100

    Refactor: centralisation des données dans OlympicService.

---
+++ src/app/core/services/olympic.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { Olympic } from '../models/olympic.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OlympicService {
+  private apiUrl = 'assets/mock/olympic.json';
+
+  constructor(private http: HttpClient) { }
+
+  getOlympics(): Observable<Olympic[]> {
+    return this.http.get<Olympic[]>(this.apiUrl);
+  }
+
+  getCountryById(id: number): Observable<Olympic | undefined> {
+    return this.getOlympics().pipe(
+      map(data => data.find(c => c.id === id))
+    );
+  }
+
+  getCountryByName(countryName: string): Observable<Olympic | undefined> {
+    return this.getOlympics().pipe(
+      map(data => data.find(c => c.country === countryName))
+    ); 
+  }
+}
